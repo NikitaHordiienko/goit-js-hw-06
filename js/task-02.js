7afb071eb0c9b9d97f0234c47ab0ee379327a81d
NikitaHordiienko <nikita.gordienko34@gmail.com>
2022-08-31 18:57:54 +0300

    Готовое 2 задание

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,23 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+
+const ingredientsListEl = document.querySelector('#ingredients');
+
+// const ingredientsItemsEl = ingredients.forEach(ingredient => {
+//   let ingredientsItemEl = document.createElement('li');
+//   ingredientsItemEl.classList.add('item');
+//   ingredientsItemEl.textContent = ingredient;
+//   ingredientsListEl.append(ingredientsItemEl);  
+// })
+
+const ingredientsItemsEl = ingredients.map((ingredient) => {
+  const ingredientsItemEl = document.createElement("li");
+  ingredientsItemEl.classList.add("item");
+  ingredientsItemEl.textContent = ingredient;  
+  return ingredientsItemEl;
+})
+
+ingredientsListEl.append(...ingredientsItemsEl);
