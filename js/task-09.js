7215497a5ec277e93938f2d2d172aedfa723d181
NikitaHordiienko <nikita.gordienko34@gmail.com>
2022-09-01 17:40:40 +0300

    Готовое 9 задание

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeColorButtonEl = document.querySelector('.change-color');
+const colorValueEl = document.querySelector('.color');
+const backgroundColorEl = document.querySelector('body');
+
+changeColorButtonEl.addEventListener('click', event => {
+  let randomColor = getRandomHexColor();
+  backgroundColorEl.style.backgroundColor = randomColor;
+  colorValueEl.textContent = randomColor;
+})
+
