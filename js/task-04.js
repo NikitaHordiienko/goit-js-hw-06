cfcfdb7b682fc5c1fe790dac5597c2db907b3a10
NikitaHordiienko <nikita.gordienko34@gmail.com>
2022-09-01 11:27:19 +0300

    Готовое 4 задание

---
+++ js/task-04.js
@@ -0,0 +1,14 @@
+const decrementButton = document.querySelector('#counter button[data-action="decrement"]');
+const incrementButton = document.querySelector('#counter button[data-action="increment"]')
+let value = document.querySelector('#value')
+let counterValue = 0;
+
+decrementButton.addEventListener('click', () => {
+    counterValue -= 1;
+    value.textContent = counterValue;
+})
+
+incrementButton.addEventListener('click', () => {
+    counterValue += 1;
+    value.textContent = counterValue;
+})
