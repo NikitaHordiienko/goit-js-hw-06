379a138ce158e75e40ddd4cfe6e654023469ac07
NikitaHordiienko <nikita.gordienko34@gmail.com>
2022-09-01 17:14:27 +0300

    Готовое 8 задание

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+const formEl = document.querySelector('.login-form');
 
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    // if (event.currentTarget.email.value === '' || event.currentTarget.password.value === '') {
+    //     return alert('All fields must be filled out')
+    // }
+
+    const { email, password } = event.currentTarget.elements;
+
+    if (email.value === '' || password.value === '') {
+        return alert('All fields must be filled out')
+    };
+
+    const formDataEl = {
+        email: email.value,
+        password: password.value,
+    };
+
+    console.log(formDataEl);
+    event.currentTarget.reset();
+}
